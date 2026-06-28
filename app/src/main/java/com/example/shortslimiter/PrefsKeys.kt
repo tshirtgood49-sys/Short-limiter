c4ded59bb59f84acb33066fca5922190f2e71ecf
tshirtgood49-sys <tshirtgood49@gmail.com>
2026-06-28 21:14:21 +0530

    Update PrefsKeys.kt

--- app/src/main/java/com/example/shortslimiter/PrefsKeys.kt
+++ app/src/main/java/com/example/shortslimiter/PrefsKeys.kt
@@ -5,6 +5,8 @@ object PrefsKeys {
     const val KEY_COUNT = "shorts_count"
     const val KEY_DATE = "shorts_date"
     const val KEY_LIMIT = "shorts_limit"
+    const val KEY_RESET_USES_COUNT = "reset_uses_count"
+    const val KEY_RESET_USES_DATE = "reset_uses_date"
     const val KEY_BLOCKED_UNTIL = "blocked_until"
 
     const val DEFAULT_LIMIT = 5
